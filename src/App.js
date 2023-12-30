281c37c486236fe39f35aed622caf17dd636af2c
Shreyeska <silwalshreyeska@gmail.com>
2023-12-30 09:20:12 +0545

    Classwork Day1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+//import Main from './Main';
+//import ClassParent from './Class';
+import Display from './Display';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Main/> */}
+      {/* <ClassParent/> */}
+      <Display/>
+    </div>
+  );
+}
+
+export default App;
